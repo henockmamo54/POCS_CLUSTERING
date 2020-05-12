5a3ea0e204899631475d4397c675b197cd318370
DESKTOP-3BGEHE7\Henock <henockmamo54@gmail.com>
2020-05-12 15:00:29 +0900

    experiment on bench mark dataset for syntheic dataset

---
+++ Experiments/Spyder_BenchmarkDataset-syntetic - FCM - KMeans/a2/_Kmeans.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 11 20:44:11 2020
+
+@author: Henock
+"""
+
+# iris data Kmeans test 
+
+import numpy as np
+import random
+import math
+from matplotlib import pyplot as plt
+from sklearn.datasets.samples_generator import make_blobs
+import time
+import pandas as pd
+from scipy.spatial.distance import cdist
+from sklearn.utils import shuffle 
+import numpy as np
+import kmeansAlgo as k
+from sklearn.preprocessing import LabelEncoder
+from sklearn.utils import shuffle
+np.random.seed(10)
+import pandas as pd
+import numpy as np
+from sklearn import metrics as m
+from sklearn.preprocessing import MinMaxScaler
+from scipy.spatial.distance import cdist
+from scipy.spatial import distance
+
+
+cluster=35
+data=pd.read_csv('../../../dataset/a2.txt', header=None, sep=',')  #
+datasetname='a2'
+scaler=MinMaxScaler()
+data=pd.DataFrame(scaler.fit_transform(data))
+x= np.asarray(data.iloc[:, :])  
+data.head()  
+
+error=[]
+
+for i in range(10):
+    
+    data=shuffle(data)
+    x= np.asarray(data.iloc[:, :])    
+
+    kmeans = k.Kmeans(n_clusters=cluster, random_state=5,max_iter=50)
+    kmeans.fit(x)
+    
+    labels=kmeans.labels
+    centers=kmeans.centroids
+    dst=0
+    
+    for j in range(len(labels)):
+        dst+=distance.euclidean(list(data.iloc[j,:]),centers[labels[j]]) 
+    error.append(dst)
+    
+    print(i)
+    
+
+print(error)
+print('mean ',np.mean(error))
+print('std ',np.std(error))
